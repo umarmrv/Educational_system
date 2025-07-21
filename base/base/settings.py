900b5a27426adc0a9e5d5b3f75fbb0cdd85057ad
Shohjahon <sh.murodovv@gmail.com>
2025-07-21 14:42:17 +0500

    created all the models and set up settings.py part

--- base/base/settings.py
+++ base/base/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Education.apps.EducationConfig'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+AUTH_USER_MODEL = 'Education.User'
