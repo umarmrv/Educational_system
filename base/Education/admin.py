0d0e024db824e8e4ed002b4ebfdf11933d617258
Shohjahon <sh.murodovv@gmail.com>
2025-07-21 14:48:44 +0500

    registered models on admin part

--- base/Education/admin.py
+++ base/Education/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User, Course, Group
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Group)
+
