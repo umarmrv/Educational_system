3426a4d7350817264f76dabd2b767e9ab59ad790
Shohjahon <sh.murodovv@gmail.com>
2025-07-21 15:14:41 +0500

    for the admin part  made vizible only studens list

--- base/Education/admin.py
+++ base/Education/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from .models import User, Course, Group
+from .forms import GroupAdminForm
+
+
+
+
+class GroupAdmin(admin.ModelAdmin):
+    form = GroupAdminForm
 
 admin.site.register(User)
 admin.site.register(Course)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
+
+
+
 
