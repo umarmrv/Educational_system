33499720db56e0bb744375edacd284ce45aaffcd
Shohjahon <sh.murodovv@gmail.com>
2025-09-10 14:56:29 +0500

    some

--- Education/models.py
+++ Education/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils    import timezone
+
 # Роли пользователей
 class Role(models.TextChoices):
     ADMIN = 'admin', 'Админ'
